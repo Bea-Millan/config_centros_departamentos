3c3d63abc137588e58b08ea81633365d4db430c7
Antoniogimeno <antoniog@unizar.es>
2022-03-02 14:31:19 +0100

    Primer commit de veterinaria

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'type' => 'drupal-config',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'unizar/config_centros_departamentos',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'unizar/config_centros_departamentos' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'type' => 'drupal-config',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
